95f4e151e72515650c321df76f4bf312d51f09b0
Zi Wang <wangzi0112@gmail.com>
2019-03-25 22:00:04 -0700

    create trie

---
+++ src/utils/trie.ts
@@ -0,0 +1,43 @@
+export default class TrieNode<V> {
+
+  private readonly values: V[] = [];
+  private readonly nodes: { [key: string]: TrieNode<V> } = {};
+
+  insert(key: string, value: V){
+    if(!value || !key) {
+      return;
+    }
+
+    const prefix = key.toLowerCase().split('');
+    let root: TrieNode<V> = this;
+    prefix.forEach(char => {
+      root.nodes[char] = root.nodes[char] || new TrieNode<V>();
+      root = root.nodes[char];
+    });
+    root.values.push(value);
+  }
+
+  find(key: string, exact = false): V[] {
+    if(!key){
+      return [];
+    }
+    const prefix = key.toLowerCase().split('');
+    let root: TrieNode<V> = this;
+
+    prefix.forEach(char => {
+      root.nodes[char] = root.nodes[char] || new TrieNode<V>();
+      root = root.nodes[char];
+    });
+
+    return exact ? root.values : root.getAllItemsFromNode();
+  }
+
+  private getAllItemsFromNode(): V[] {
+    let results = this.values;
+    Object.keys(this.nodes).forEach(char => {
+      results = results.concat(this.nodes[char].getAllItemsFromNode());
+    });
+    return results;
+  }
+
+}
