934bc4f7e62a5432fad7a3b95f3582910d850562
Zi Wang <wangzi0112@gmail.com>
2019-06-23 10:22:31 -0700

    update dependency

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
 
 Vue.component('v-select', require('vue-select/dist/vue-select').VueSelect);
+Vue.use(Antd);
 
 new Vue({
   router,
