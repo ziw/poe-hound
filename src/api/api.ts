ce9ccd04aa7c099fbded84b8dd9cbbcf8d9c6aad
Zi Wang <wangzi0112@gmail.com>
2019-06-25 20:30:10 -0700

    add a temp solution to cache API response for offline use case

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,8 +3,12 @@ import  request, {FullResponse} from 'request-promise-native';
 import Character from '@/models/character';
 import StashPage from '@/models/stashPage';
 import Item from '@/models/item';
+import OfflineCache from '@/utils/offlineCache';
+
+const offlineCacher = new OfflineCache(process.env.VUE_APP_CACHE_DIR as string);
 
 const get = (url: string, sessionId: string)=> {
+
   return request.get({
     url,
     headers: {
@@ -12,14 +16,25 @@ const get = (url: string, sessionId: string)=> {
     },
     rejectUnauthorized: true,
     resolveWithFullResponse: true,
-  }).then(resp => {
+  })
+  .then((resp: FullResponse) => {
+    if(resp.statusCode === 200){
+      offlineCacher.cache(url, resp.body);
+    }
     console.log({
       resp,
       url,
     });
     return resp;
+  }).catch(error => {
+    if(process.env.VUE_APP_CACHE_DIR){
+      return offlineCacher.read(url).then(content => ({
+        body: content!
+      }));
+    }
+    throw error;
   });
-}
+};
 
 const buildUrl = (path: string, queryObject?: any) => {
   const url = new URL(PATHS.baseUrl + path);
@@ -33,17 +48,17 @@ const buildUrl = (path: string, queryObject?: any) => {
 
 export function authenticate(sessionId: string): Promise<string> {
   return get(buildUrl(PATHS.accountNameUrl), sessionId)
-          .then((resp: FullResponse) => JSON.parse(resp.body).accountName);
+          .then((resp: {body: string}) => JSON.parse(resp.body).accountName);
 }
 
 export function loadCharacters(sessionId: string) {
   return get(buildUrl(PATHS.charactersUrl), sessionId)
-          .then((resp: FullResponse) => JSON.parse(resp.body) as Character[]);
+          .then((resp: {body: string}) => JSON.parse(resp.body) as Character[]);
 }
 
 export function loadInventory(sessionId: string, character: string, accountName: string){
   return get(buildUrl(PATHS.inventoryUrl, { character, accountName }), sessionId)
-          .then((resp: FullResponse) => (JSON.parse(resp.body) as { items: Item[] }).items);
+          .then((resp: {body: string}) => (JSON.parse(resp.body) as { items: Item[] }).items);
 }
 
 export function loadLeagueStashInformation(sessionId: string, league: string, accountName: string){
@@ -53,5 +68,5 @@ export function loadLeagueStashInformation(sessionId: string, league: string, ac
     accountName,
   };
   return get(buildUrl(PATHS.stashInLeagueUrl, query), sessionId)
-          .then((resp: FullResponse) => JSON.parse(resp.body).tabs as StashPage[]);
+          .then((resp: {body: string}) => JSON.parse(resp.body).tabs as StashPage[]);
 }
---
+++ src/utils/offlineCache.ts
@@ -0,0 +1,46 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+const toFileName = (requestUrl: string): string | undefined => {
+  try{
+    const url = new URL(requestUrl);
+    const search = url.search.replace(/\?/g, '')
+                      .replace(/=/g, '--')
+                      .replace(/&/g, '#');
+    const paths = url.pathname.split('/').filter(seg => seg.trim().length).join('__');
+    return `${paths}-${search}.json`;
+  }catch{
+    return undefined;
+  }
+}
+
+export default class OfflineCache {
+  constructor(
+    public cacheDir: string
+  ){
+  }
+
+  setCacheDir(dir: string) {
+    this.cacheDir = dir;
+    return this;
+  }
+
+  async cache(fullRequestUrl: string, content: string) {
+    const fileName = toFileName(fullRequestUrl);
+    if(fileName && await this.isCacheDirValid()) {
+      return fs.writeFile(path.join(this.cacheDir, fileName), content);
+    }
+  }
+
+  async read(fullRequestUrl: string) {
+    const fileName = toFileName(fullRequestUrl);
+    if(fileName && await this.isCacheDirValid()){
+      return fs.readFile(path.join(this.cacheDir, fileName), 'utf8');
+    }
+  }
+
+  private isCacheDirValid() {
+    return fs.stat(this.cacheDir)
+             .then(stats => stats.isDirectory());
+  }
+}
